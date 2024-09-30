2d65aecfcb82ec96f8d98b6f7461ec9ead5b4ac8
spincycle <99446106+JonathanBurg@users.noreply.github.com>
2024-09-30 09:32:19 -0600

    Add project files.

---
+++ fibSer.asm
@@ -0,0 +1,161 @@
+ ; Fibonacci Series
+ ; Jonathan Burgener
+ ; Monday, 30 September, 2024
+ ; Prints the numbers in the Fibonacci Series up to a user-inputted number.
+ ; 
+ ; Registers Used:
+ ;		EAX - Store current number to push to console
+ ;		EDX - Temporarily store number for moving data
+
+.model flat
+
+extern charCount:	near
+extern readline:	near
+extern writeline:   near
+extern writeNumber: near
+
+.data
+
+	var1			DD	 ? ; first number to add
+	var2			DD	 ? ; second number to add
+	var3			DD	 ? ; term to stop at: From user input
+	itr				DD	 ? ; current term of the series
+	prompt			byte "Please enter max number (<45): ", 0 ; ends with string terminator (NULL or 0)
+	results         byte  10,"You typed: ", 0
+	numCharsToRead  dword 1024
+	bufferAddr		dword ?
+
+.code
+
+ ; Gets number from user input and checks that it is less than 45
+getNumber PROC near
+_getNumber:
+
+	; loop to make sure inputted number is less than 45
+top:
+	 ; Type a prompt for the user
+     ; WriteConsole(handle, &Prompt[0], 17, &written, 0)
+	push offset prompt
+    call charCount
+    push eax
+    push offset prompt
+    call writeline
+
+	 ; Read user input
+	call findInput
+	
+	;mov eax, 10
+	 ; If input is 0, return
+	cmp  eax, 0
+	jle  exit
+	 ; check that the number is less than 45
+	mov	 var3, eax
+	sub	 eax, 45
+	cmp	 eax, 0
+	jle	 done
+	jmp	 top
+
+	 ; Move to printing series
+done:
+	call printSeries
+	ret
+	
+	 ; Return to start
+exit:
+	ret
+
+getNumber ENDP
+
+ ; Prints the Fibonacci Series
+printSeries PROC near
+_printSeries:
+	 ; Write first two terms
+	mov  var1, 1
+	mov  var2, 2
+
+	mov   eax, var1
+	push  eax
+	call  writeNumber
+	cmp	  var3, 1
+	je	  exit
+
+	mov	  eax, var2
+	push  eax
+	call  writeNumber
+	cmp   var3, 2
+	je	  exit
+
+	mov	  itr, 2
+
+	 ; Loop to print series
+top:
+	call  iterater
+	mov	  eax, var3
+	cmp	  itr, eax
+	je	  exit
+	jmp	  top
+
+exit:
+	ret
+
+printSeries ENDP
+
+ ; Routine to get user input and convert it to an integer
+findInput PROC near
+_findInput:
+	 ; Read what the user inputed
+	call  readLine
+
+	; Prints input back to user
+    ; writeline(&results[0], 12)
+    mov   bufferAddr, eax
+    push  offset results
+    call  charCount
+    push  eax
+    push  offset results
+    call  writeline
+    push  numCharsToRead
+    push  bufferAddr
+    call  writeline
+
+atoi: ; zero result found so far
+
+	; Start loop to parse input
+top:
+	;atoi:
+	;xor eax, eax ; zero a "result so far"
+	;.top:
+	;movzx ecx, byte [edx] ; get a character
+	;inc edx ; ready for next one
+	;cmp ecx, '0' ; valid?
+	;jb .done
+	;cmp ecx, '9'
+	;ja .done
+	;sub ecx, '0' ; "convert" character to number
+	;imul eax, 10 ; multiply "result so far" by ten
+	;add eax, ecx ; add in current digit
+	;jmp .top ; until done
+	;.done:
+	;ret
+
+findInput ENDP
+
+; Routine to iterate to next element in Fibonacci Series
+iterater PROC near
+_iterater:
+    ; Try print a number
+    mov   eax, var1
+    add   eax, var2
+    mov   ebx, var2   ; Make sure numbers are not lost 
+    mov   var1, ebx   ; when EAX is overwritten in
+    mov   var2, eax   ; writeNumber
+    push  eax
+    call  writeNumber
+    
+    inc   itr
+
+    ; Return to printSeries
+    ret                 
+
+iterater ENDP
+END 
---
+++ start.asm
@@ -0,0 +1,105 @@
+; Main Console Program
+; Jonathan Burgener
+; 27 September 2024
+; Calculate and display the Fibonacci Series
+; Revised: JB, 30 September 2024, Added loops and reference to fibSer.asm
+;
+; Register names:
+; Register names are NOT case sensitive eax and EAX are the same register
+; x86 uses 8 registers. EAX (Extended AX register has 32 bits while AX is
+;	the right most 16 bits of EAX). AL is the right-most 8 bits.
+; Writing into AX or AL effects the right most bits of EAX.
+;     EAX - caller saved register - usually used for communication between
+;			caller and callee.
+;     EBX - Callee saved register
+;     ECX - Caller saved register - Counter register 
+;     EDX - Caller Saved register - data, I use it for saving and restoring
+;			the return address
+;     ESI - Callee Saved register - Source Index
+;     EDI - Callee Saved register - Destination Index
+;     ESP - Callee Saved register - stack pointer
+;     EBP - Callee Saved register - base pointer.386P
+
+.model flat
+
+extern  writeline:   near
+extern  readline:    near
+extern  charCount:   near
+extern  writeNumber: near
+extern  getNumber:   near
+
+.data
+
+num1            DD    ? ; first number for each iteration
+num2            DD    ? ; second number for each iteration
+itr             DD    ? ; iterator to make sure only 45 terms are printed
+msg             byte  "Hello, World", 10, 0   ; ends with line feed (10) and NULL
+prompt          byte  "Fibonacci Series: ", 10, 0 ; ends with string terminator (NULL or 0)
+endln           byte  "    ", 10, 0
+results         byte  ? ; buffer to print vars
+numCharsToRead  dword 1024
+bufferAddr      dword ?
+
+.code
+
+; Library calls used for input from and output to the console; This is the entry procedure that does all of the testing.
+start PROC near
+_start:
+    ; Type a message for the user
+    ; WriteConsole(handle, &Prompt[0], 17, &written, 0)
+    push  offset prompt
+    call  charCount
+    push  eax
+    push  offset prompt
+    call  writeline
+
+    ; Initialize variables
+    mov   num1, 1
+    mov   num2, 2
+    
+    ; Print first two variables
+    mov   eax, num1
+    push  eax
+    call  writeNumber
+    mov   eax, num2
+    push  eax
+    call  writeNumber
+
+    mov   itr, 2
+
+     ; loop to iterate fibonacci series
+top:
+    call  iterate
+    cmp   itr, 45
+    jl    top
+    jmp   done
+
+    ; What to do once condition is met
+done:
+    call getNumber
+
+    ; And it is time to exit.
+exit:
+    ret                                     ; Return to the main program.
+
+start ENDP
+
+; Routine to iterate to next element in Fibonacci Series
+iterate PROC near
+_iterate:
+    ; Try print a number
+    mov   eax, num1
+    add   eax, num2
+    mov   ebx, num2   ; Make sure numbers are not lost 
+    mov   num1, ebx   ; when EAX is overwritten in
+    mov   num2, eax   ; writeNumber
+    push  eax
+    call  writeNumber
+    
+    inc   itr
+
+    ; Return to start
+    ret                 
+
+iterate ENDP
+END
