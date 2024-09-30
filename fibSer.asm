6b656c5166b7ee1485a4e936205c32a1db76ac55
spincycle <99446106+JonathanBurg@users.noreply.github.com>
2024-09-30 11:45:32 -0600

    Update 2 - 9/30/2024 - 11:45:30 AM - Jonathan Burgener

--- fibSer.asm
+++ fibSer.asm
@@ -2,29 +2,26 @@
  ; Jonathan Burgener
  ; Monday, 30 September, 2024
  ; Prints the numbers in the Fibonacci Series up to a user-inputted number.
- ; 
- ; Registers Used:
- ;		EAX - Store current number to push to console
- ;		EDX - Temporarily store number for moving data
 
-.model flat
+.model  flat
 
-extern charCount:	near
-extern readline:	near
-extern writeline:   near
-extern writeNumber: near
+extern  charCount:	near
+extern  readline:	near
+extern  writeline:   near
+extern  writeNumber: near
 
 .data
 
-	var1			DD	 ? ; first number to add
-	var2			DD	 ? ; second number to add
-	var3			DD	 ? ; term to stop at: From user input
-	itr				DD	 ? ; current term of the series
-	temp			byte ? ; temporarily store a byte
-	prompt			byte "Please enter max number (<45): ", 0 ; ends with string terminator (NULL or 0)
-	results         byte  10,"You typed: ", 0
-	numCharsToRead  dword 1024
-	bufferAddr		dword ?
+	var1			DD  	? ; first number to add
+	var2			DD  	? ; second number to add
+	var3			DD  	? ; term to stop at: From user input
+	itr				DD  	? ; current term of the series
+	temp			byte	? ; temporarily store a byte
+	prompt			byte	10,"Please enter max number (<45): ", 0 ; ends with string terminator (NULL or 0)
+	msg				byte	10,"Fibonacci Series: ", 10, 0 ; ends with string terminator (NULL or 0)
+	results         byte	10,"You typed: ", 0
+	numCharsToRead  dword	1024
+	bufferAddr		dword	?
 
 .code
 
@@ -36,27 +33,31 @@ _getNumber:
 top:
 	 ; Type a prompt for the user
      ; WriteConsole(handle, &Prompt[0], 17, &written, 0)
-	push offset prompt
-    call charCount
-    push eax
-    push offset prompt
-    call writeline
+	push  offset prompt
+    call  charCount
+    push  eax
+    push  offset prompt
+    call  writeline
 
 	 ; Read user input
-	call findInput
-	mov eax, var3
-	 ; If input is 0, return
-	cmp  eax, 0
-	jle  exit
+	call  findInput
+	mov   eax, var3
+
+	;push eax
+	;call writeNumber
+	;mov eax, var3
 	 ; check that the number is less than 45
 	mov	 var3, eax
-	sub	 eax, 45
-	cmp	 eax, 0
-	jle	 done
-	jmp	 top
+	cmp	 eax, 45
+	jge	 top
+	cmp  eax, 0
+	jl	 top
+	jmp	 done
 
 	 ; Move to printing series
 done:
+	push eax
+	call writeNumber
 	call printSeries
 	ret
 	
@@ -66,9 +67,84 @@ exit:
 
 getNumber ENDP
 
+
+ ; Routine to get user input and convert it to an integer
+findInput PROC near
+_findInput:
+	 ; Read what the user inputed
+	call  readLine
+
+	; Prints input back to user
+    ; writeline(&results[0], 12)
+    mov   bufferAddr, eax
+    push  offset results
+    call  charCount
+    push  eax
+    push  offset results
+    call  writeline
+    push  numCharsToRead
+    push  bufferAddr
+    call  writeline
+
+	 ; Preserve working registers
+	push  ecx ; Current character
+	push  ebx ; Base data
+	push  edx ; Destination register
+
+	mov  ebx, eax
+atoi: ; Zero result found so far
+	xor  edx, edx
+
+top: ; Start loop to parse input
+	xor   ecx, ecx 	; Clear ECX
+	movzx ecx, bl 	; Get a character
+	inc   ebx		; Increment base value
+	cmp   cl, '0'	
+	jb    fin		; Does the character precede '0'?
+	cmp   cl, '9'	
+	ja    fin		; Does the character follow '9'?
+	sub   cl, '0'	; Cast character to number
+	imul  edx, 10	; Shift saved digits to the left
+	add	  edx, ecx	; Add new digit to number
+	jmp   top		; Until done
+
+	;movzx ecx, byte [edx] ; get a character
+	;inc edx ; ready for next one
+	;cmp ecx, '0' ; valid?
+	;jb .done
+	;cmp ecx, '9'
+	;ja .done
+	;sub ecx, '0' ; "convert" character to number
+	;imul eax, 10 ; multiply "result so far" by ten
+	;add eax, ecx ; add in current digit
+	;jmp .top ; until done
+	
+fin:
+	mov   var3, edx
+	push  edx
+	call  writeNumber
+	
+	 ; Restore working data
+	pop  edx
+	pop  ebx
+	pop  ecx
+
+	ret ; Return to getNumber
+
+findInput ENDP
+
+
  ; Prints the Fibonacci Series
 printSeries PROC near
 _printSeries:
+	; Type a message for the user
+    ; WriteConsole(handle, &Prompt[0], 17, &written, 0)
+    push  offset msg
+    call  charCount
+    push  eax
+    push  offset msg
+    call  writeline
+
 	 ; Write first two terms
 	mov  var1, 1
 	mov  var2, 2
@@ -92,74 +168,17 @@ top:
 	call  iterater
 	mov	  eax, var3
 	cmp	  itr, eax
-	je	  exit
-	jmp	  top
+	jge	  exit	; Has the term index reached the set amount?
+
+	cmp   itr, 45
+	jge   exit	; Has the term index reached the limit?
+	jmp	  top	; Until done
 
 exit:
 	ret
 
 printSeries ENDP
 
- ; Routine to get user input and convert it to an integer
-findInput PROC near
-_findInput:
-	 ; Read what the user inputed
-	call  readLine
-
-	; Prints input back to user
-    ; writeline(&results[0], 12)
-    mov   bufferAddr, eax
-    push  offset results
-    call  charCount
-    push  eax
-    push  offset results
-    call  writeline
-    push  numCharsToRead
-    push  bufferAddr
-    call  writeline
-
-	push ecx ; Preserve working registers
-	push edx
-
-	xor ecx, ecx
-	mov edx, eax
-atoi: ; Zero result found so far
-	xor eax, eax
-
-top: ; Start loop to parse input
-	mov cl, dl ; Get a character
-	inc  edx
-	cmp  ecx, '0'
-	jb   fin
-	cmp  ecx, '9'
-	ja   fin
-	sub  ecx, '9'
-	ja   fin
-	sub  ecx, '0' ; Cast character to number
-	imul eax, 10
-	add	 eax, ecx 
-	jmp  top ; Until done
-
-	;movzx ecx, byte [edx] ; get a character
-	;inc edx ; ready for next one
-	;cmp ecx, '0' ; valid?
-	;jb .done
-	;cmp ecx, '9'
-	;ja .done
-	;sub ecx, '0' ; "convert" character to number
-	;imul eax, 10 ; multiply "result so far" by ten
-	;add eax, ecx ; add in current digit
-	;jmp .top ; until done
-	
-fin:
-	pop edx
-	pop ecx
-	mov var3, eax
-	push eax
-	call writeNumber
-	ret ; Return to getNumber
-
-findInput ENDP
 
 ; Routine to iterate to next element in Fibonacci Series
 iterater PROC near
