128716290ba9484cea6a4a6b5366849a6675bbaf
spincycle <99446106+JonathanBurg@users.noreply.github.com>
2024-10-17 09:19:39 -0600

    Start of Version 2 - 10/17/2024 - 9:19 AM - Jonathan Burgener
    Adding Headers and new output line

--- fibSer.asm
+++ fibSer.asm
@@ -2,8 +2,9 @@
  ; Jonathan Burgener
  ; Monday, 30 September, 2024
  ; Prints the numbers in the Fibonacci Series up to a user-inputted number.
- ;
- ; Revised: JB, 7 October, 2024 - Finally got a reliable way to convert a string to an int; Removed debug messages
+ ; 
+ ; Revised: JB,  7 October, 2024 - Finally got a reliable way to convert a string to an int; Removed debug messages
+ ; Revised: JB, 16 October, 2024 - Updated headers and added final statement to user
 
 .model  flat
 
@@ -12,25 +13,34 @@ extern  readline:	 near
 extern  writeline:	 near
 extern  writeNumber: near
 extern	writeln:	 near
+extern	readInt:	 near
 
 .data
 
-	var1			DD  	? ; first number to add
-	var2			DD  	? ; second number to add
-	var3			DD  	? ; term to stop at: From user input
-	itr				DD  	? ; current term of the series
+	var1			DD		? ; first number to add
+	var2			DD		? ; second number to add
+	var3			DD		? ; term to stop at: From user input
+	itr				DD		? ; current term of the series
 	temp			byte	? ; temporarily store a byte
 	prompt			byte	"Please enter max number (<45): ", 0	; ends with string terminator (NULL or 0)
-	error			byte	"Error, Invalid Number",10,0				
-	msg				byte	10,"Fibonacci Series: ", 10, 0				; ends with string terminator (NULL or 0)
-	results         byte	10,"You typed: ", 0
+	error			byte	"Error, Invalid Number",10,0
+	msg				byte	10,"Fibonacci Series: ", 10, 0			; ends with string terminator (NULL or 0)
+	results			byte	10,"You typed: ", 0
 	input			dword	?
-	numCharsToRead  dword	1024
+	numCharsToRead	dword	1024
 	bufferAddr		dword	?
 
 .code
 
- ; Gets number from user input and checks that it is less than 45
+;;******************************************************************;
+;; Call getNumber()
+;; Parameters:		None
+;; Returns:			nothing
+;; Registers Used:	EAX
+;; 
+;; Controller for finding the Fibonacci Series
+;;		to the user defined n-th term
+;;******************************************************************;
 getNumber PROC near
 _getNumber:
 	call writeln
@@ -38,9 +48,11 @@ _getNumber:
 
  ; loop to make sure inputted number is less than 45
 top:
-	
 	 ; Read user input
-	call  findInput
+	;call  findInput
+	push  offset prompt
+	call  readInt
+	mov   var3, eax
 
 	 ; check that the number is less than 45
 	mov	 eax, var3
@@ -49,7 +61,7 @@ top:
 	cmp  eax, 0
 	jl	 invalidInput
 	jmp	 done
-
+	
  ; Move to printing series
 done:
 	call printSeries
@@ -57,79 +69,26 @@ done:
  ; Return to start
 exit:
 	ret
-
+	
+ ; Type an error for the user
 invalidInput:
-	; Type an error for the user
 	push  offset error
 	call  charCount
-    push  eax
-    push  offset error
-    call  writeline
-	jp	  top
+	push  eax
+	push  offset error
+	call  writeline
+	jp    top
 getNumber ENDP
 
- ;*****************************************************************
- ; Routine to get user input and convert it to an integer
- ; Source of delay in completion, was struggling to find a way to
- ;  	reliably convert a string to an integer
- ; Routine written by Wayne Cook
- ; Adapted by Jonathan Burgener to fit program
- ;*****************************************************************
-findInput PROC near
-_findInput:
-	 ; Store working registers
-	push  eax
-	push  ebx
-	push  ecx
-	push  edx
-	xor   eax, eax
-	xor   ebx, ebx
-	xor   ecx, ecx
-	xor   edx, edx
 
-	; Type a prompt for the user
-	push  offset prompt
-    call  charCount
-    push  eax
-    push  offset prompt
-    call  writeline
-
-	call  readline
-	mov   ecx, eax
-
- ; Take what was read and convert to a number
-    mov   eax, 0			; Initialize the number
-    mov   ebx, 0			; Make sure upper bits are all zero.
-    
-findNumberLoop:
-    mov   bl, [ecx]			; Load the low byte of the EBX reg with the next ASCII character.
-    cmp   bl, '9'			; Make sure it is not too high
-    jg   endNumberLoop
-    sub   bl, '0'               
-    cmp   bl, 0				; or too low
-    jl    endNumberLoop
-    mov   edx, 10			; save multiplier for later need
-    mul   edx
-    add   eax, ebx
-    inc   ecx				; Go to next location in number
-    jmp   findNumberLoop
-
-endNumberLoop:
- ; var3 has the new integer when the code completes. And ) if no digits found.
-	mov var3, eax
-
- ; Restore working registers
-	pop edx
-	pop ecx
-	pop ebx
-	pop eax
-
- ; Return to getNumber
-	ret
-findInput ENDP
-
-
- ; Prints the Fibonacci Series
+;;******************************************************************;
+;; Call findInput(var3)
+;; Parameters:	var3 - User input integer, series length
+;; Returns:		EAX  - Value of n-th term (var2)
+;; Registers Used:	EAX
+;; 
+;; Prints the Fibonacci Series to the user defined n-th term
+;;******************************************************************;
 printSeries PROC near
 _printSeries:
 	cmp   var3, 0
@@ -141,11 +100,11 @@ _printSeries:
 
 print:
 	; Type a message for the user
-    push  offset msg
-    call  charCount
-    push  eax
-    push  offset msg
-    call  writeline
+	push  offset msg
+	call  charCount
+	push  eax
+	push  offset msg
+	call  writeline
 
 	 ; Write first two terms
 	mov   var1, 1
@@ -177,6 +136,7 @@ top:
 	jmp	  top	; Until done
 
 exit:
+	mov eax, var2
 	ret
 
 invalidlimit:
@@ -186,22 +146,30 @@ invalidlimit:
 printSeries ENDP
 
 
-; Routine to iterate to next element in Fibonacci Series
+;;******************************************************************;
+;; Call iterater(var1, var2, itr)
+;; Parameters:	var1 - value of previous term	(called from memory)
+;;				var2 - value of latest term		(called from memory)
+;;				itr  - current term				(called from memory)
+;; Returns:		Nothing
+;; Registers Used:	EAX, EBX
+;; 
+;; Routine to iterate to next element in Fibonacci Series
+;;******************************************************************;
 iterater PROC near
 _iterater:
 	 ; Try to print a number
-    mov   eax, var1
-    add   eax, var2
-    mov   ebx, var2		; Make sure numbers are not lost 
-    mov   var1, ebx		; when EAX is overwritten in
-    mov   var2, eax		; writeNumber
-    push  eax
-    call  writeNumber
-    
-    inc   itr
+	mov   eax, var1
+	add   eax, var2
+	mov   ebx, var2		; Make sure numbers are not lost 
+	mov   var1, ebx		; when EAX is overwritten in
+	mov   var2, eax		; writeNumber
+	push  eax
+	call  writeNumber
+	
+	inc   itr
 
 	 ; Return to printSeries
-    ret                 
-
+	ret
 iterater ENDP
 END 
--- main.asm
+++ main.asm
@@ -6,7 +6,8 @@
 ; Revised 15 March 2024 Added this comment to force a new commit.
 ; Revised: WWC 25 April 2024 Split out the console control into second file.
 ; Revised: WWC 13 September 2024 Minor updates for Fall 2024 semester.
-; Revised: JB,  7 October, 2024, Added exit message
+; Revised: JB,  7 October, 2024 - Added exit message
+; Revised: JB, 16 October, 2024 - Added headers
 ; Register names:
 ; Register names are NOT case sensitive eax and EAX are the same register
 ; x86 uses 8 registers. EAX (Extended AX register has 32 bits while AX is the right most 16 bits of EAX). AL is the right-most 8 bits.
@@ -23,17 +24,25 @@
 .386P
 
 .model	flat
-extern	_ExitProcess@4: near
-extern	initialize_console: near
-extern	start: near
+extern	_ExitProcess@4:		near
+extern	initialize_console:	near
+extern	start:		near
 extern  charCount:	near
-extern  writeline:   near
+extern  writeline:	near
 
 .data  
 	exitmsg 	byte	10,10,10,"Hello World!",0 ; Exit message
 
 .code
 
+;;******************************************************************;
+;; Call 
+;; Parameters:		
+;; Returns:			
+;; Registers Used:	
+;; 
+;; Description
+;;******************************************************************;
 main PROC near
 _main:
 	call initialize_console
@@ -41,6 +50,15 @@ _main:
 	call exit
 main ENDP
 
+
+;;******************************************************************;
+;; Call exit()
+;; Parameters:		None
+;; Returns:			Nothing
+;; Registers Used:	EAX
+;; 
+;; Exits the program with a message to the user.
+;;******************************************************************;
 exit PROC near
 _exit:
 	; Write an exit message for the user
@@ -50,7 +68,7 @@ _exit:
     push  offset exitmsg
     call  writeline
 
-		 ; ExitProcess(uExitCode)
+	 ; ExitProcess(uExitCode)
     push  5
     call  _ExitProcess@4
 exit ENDP
